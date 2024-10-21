0c576a76d43f486969b4b6469002823066f563a2
limkeunhyeok <gorloom6425@naver.com>
2024-10-21 23:53:55 +0900

    fix: seperate config logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,93 +1,39 @@
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Inject, Logger, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { JwtModule } from '@nestjs/jwt';
-import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
-import * as depthLimit from 'graphql-depth-limit';
-import { Connection } from 'mongoose';
-import * as path from 'path';
 import { AuthGuard } from './common/guards/auth.guard';
 import { HealthModule } from './common/health/health.module';
-import config from './config';
-import {
-  AUTH_SOURCE,
-  MONGO_DB,
-  MONGO_HOST,
-  MONGO_NAME,
-  MONGO_PASS,
-  MONGO_PORT,
-  MONGO_USER,
-} from './constants/database.const';
+import { GraphqlConfigService } from './configs/graphql.config.service';
+import { JwtConfigService } from './configs/jwt.config.service';
+import { MongodbConfigService } from './configs/mongo.config.service';
+import serverConfig from './configs/server.config';
 import { Role } from './constants/role.const';
-import { SECRET_KEY } from './constants/server.const';
 import { ApiModule } from './modules/api/v2/api.module';
 import { UserServicePort } from './modules/api/v2/users/ports/in/user.service.port';
 import { USER_SERVICE } from './modules/api/v2/users/user.const';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [config] }),
+    ConfigModule.forRoot({ load: [serverConfig] }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: `${MONGO_DB}://${configService.get(
-          MONGO_HOST,
-        )}:${configService.get(MONGO_PORT)}/${configService.get(MONGO_NAME)}`,
-        authSource: AUTH_SOURCE,
-        user: configService.get(MONGO_USER),
-        pass: configService.get(MONGO_PASS),
-      }),
+      useClass: MongodbConfigService,
       inject: [ConfigService],
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: false,
-      debug: true,
-      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      introspection: true,
-      validationRules: [depthLimit(3)], // Paginate의 경우가 있어 3으로 제한
-      formatError: (error) => {
-        const { message, path, extensions } = error;
-        // 일단, 아폴로 자체 에러는 400으로 처리
-        const statusCode = extensions.originalError
-          ? (extensions.originalError as any).statusCode
-          : 400;
-        const exceptionCode = extensions.code;
-        const stack = extensions.stacktrace;
-        const timestamp = new Date();
-
-        return {
-          message: 'An error occurred.',
-          path,
-          extensions: {
-            exceptionCode,
-            statusCode,
-            message,
-            stack,
-            timestamp,
-          },
-        };
-      },
+      useClass: GraphqlConfigService,
     }),
     ApiModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return {
-          global: true,
-          secret: configService.get(SECRET_KEY),
-          signOptions: {
-            expiresIn: '1h',
-          },
-        };
-      },
+      useClass: JwtConfigService,
     }),
     HealthModule,
   ],
@@ -95,14 +41,9 @@ import { USER_SERVICE } from './modules/api/v2/users/user.const';
   exports: [Logger],
 })
 export class AppModule implements OnModuleInit {
-  private logger: Logger;
-
   constructor(
     @Inject(USER_SERVICE) private readonly userService: UserServicePort,
-    @InjectConnection() private connection: Connection,
-  ) {
-    this.logger = new Logger(this.constructor.name);
-  }
+  ) {}
 
   async onModuleInit() {
     const email = 'admin@example.com';
@@ -114,26 +55,6 @@ export class AppModule implements OnModuleInit {
     if (!hasAdmin) {
       await this.userService.create({ email, password, name, role });
     }
-
-    this.connection.on('connected', () => {
-      this.logger.log('MongoDB connected');
-    });
-
-    this.connection.on('disconnected', () => {
-      this.logger.warn('MongoDB disconnected');
-    });
-
-    this.connection.on('reconnected', () => {
-      this.logger.log('MongoDB reconnected');
-    });
-
-    this.connection.on('error', (error) => {
-      this.logger.error(`MongoDB connection error: ${error}`);
-    });
-
-    this.connection.on('close', () => {
-      this.logger.log('MongoDB connection closed');
-    });
   }
 }
 
--- src/configs/server.config.ts
+++ src/configs/server.config.ts
@@ -1,11 +1,16 @@
 export default () => ({
+  // Server
   nodeEnv: process.env.NODE_ENV || 'dev',
   port: Number(process.env.PORT) || 3333,
+
+  // DB
   mongoHost: process.env.MONGO_HOST || 'localhost',
   mongoPort: Number(process.env.MONGO_PORT) || 27017,
   mongoUser: process.env.MONGO_USER || 'root',
   mongoPass: process.env.MONGO_PASS || 'password',
   mongoName: process.env.MONGO_NAME || 'graphql',
+
+  // auth
   secretKey: process.env.SECRET_KEY || 'secretKey',
   saltRound: Number(process.env.SALT_ROUND) || 10,
 });
