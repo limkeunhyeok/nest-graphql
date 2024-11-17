12c7f0211e45c7350c2d7e46516383a615e6ffc6
limkeunhyeok <gorloom6425@naver.com>
2024-11-17 19:12:17 +0900

    feat: add db scripts

---
+++ scripts/db-dump.sh
@@ -0,0 +1,5 @@
+mongodump \
+   --host=mongo1:27017,mongo2:27018,mongo3:27019 \
+   --username=root \
+   --password="password" \
+   --out=//Users/limkeunhyeok/Desktop/graphql/mongodump-1
---
+++ scripts/db-restore.sh
@@ -0,0 +1,6 @@
+mongorestore \
+   --host=mongo1:27017,mongo2:27018,mongo3:27019 \
+   --username=root \
+   --password="password" \
+   --authenticationDatabase=admin \
+   /Users/limkeunhyeok/Desktop/graphql/mongodump-1
